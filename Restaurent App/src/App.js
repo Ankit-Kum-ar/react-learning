cb57511a5b079493e7d1c00a0cd9aecd6f6a0190
Ankit-0009 <ankitcode2511@gmail.com>
2024-07-01 02:58:19 +0530

    Updated

--- Restaurent App/src/App.js
+++ Restaurent App/src/App.js
@@ -2,19 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import { Error } from "./components/Error";
 
 // Creating AppLayout in which all three components have come.
 const AppLayout = () => {
     return (
         <>
         <Header/>
-        <Body/>
+        <Outlet/>
         {/* <Footer/> */}
         </>
     )
-}
+}   
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout/>,
+        children: [
+            {
+                path : "/",
+                element : <Body/>
+            },
+            {
+                path : "/about",
+                element : <About/>
+            },
+            {
+                path : "/contact",
+                element : <Contact/>
+            }
+        ],
+        errorElement: <Error/>
+    }
+   
+])
 
 // Rendering the react element to the root element.
 const root = ReactDOM.createRoot(document.querySelector(".root"));
-root.render(<AppLayout/>);
+root.render(<RouterProvider router={appRouter}/>);
---
+++ Restaurent App/src/components/Contact.js
@@ -0,0 +1,8 @@
+export const Contact = () => {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <p>For any queries, please contact us at: 9999666707</p>
+        </div>
+    )
+}
